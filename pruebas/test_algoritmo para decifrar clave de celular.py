2c37efc51c3d0c2765a501c2219b0ce5a952b77a
Angelica Ruge <angelicaruge518@gmail.com>
2023-05-02 14:33:45 -0500

    pruebas

---
+++ pruebas/test_algoritmo para decifrar clave de celular.py
@@ -0,0 +1,5 @@
+import pytest
+from algoritmo para decifrar clave de celular import ordenamiento_por_mezcla
+
+def test_ordenamiento_por_mezcla():
+    assert ordenamiento_por_mezcla(3,4,6,2,1,8,7,0,9)
